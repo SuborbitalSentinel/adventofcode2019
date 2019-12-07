ea24a6317cbaaf57cff0ed8704ea2d9a20c728f3
Adam Miller <millera023@gmail.com>
2019-12-07 18:05:10 -0500

    Implemented a wonky way to map the wire paths and find where they intersect

--- day3/day3.go
+++ day3/day3.go
@@ -1,26 +1,11 @@
 package day3
 
 import (
+	"fmt"
+	"math"
+	"strconv"
 	"strings"
 )
-
-func tokenizePath(input string) []string {
-	return strings.Split(input, ",")
-}
-
-type point struct {
-	X, Y int
-}
-
-func tracePath(tokens []string, wireNumber int, m map[point][]int) {
-	x := 0
-	y := 0
-	for _, token := range tokens {
-		// loop over each token using the above x and y value to add a new point to the hash map with the wire number
-		token = token + "a"
-		m[point{x, y}] = append(m[point{x, y}], wireNumber)
-	}
-}
 
 // Run day 3 of advent
 func Run(input <-chan string) {
@@ -31,5 +16,90 @@ func Run(input <-chan string) {
 		tokens := tokenizePath(wirePath)
 		tracePath(tokens, wireNumber, m)
 		wireNumber++
+	}
+
+	// point 0,0 will always have the wires connected
+	delete(m, point{0, 0})
+
+	minSeenDistance := math.MaxInt32
+	for key, value := range m {
+		if len(value) == 2 { // Assuming that there were be at most 2 values per point in space
+			if value[0] != value[1] {
+				distance := abs(key.X) + abs(key.Y)
+				if distance < minSeenDistance {
+					minSeenDistance = distance
+				}
+			}
+		}
+	}
+
+	fmt.Println("Day3 -- Part1: ", minSeenDistance)
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
+}
+
+type point struct {
+	X, Y int
+}
+
+func tokenizePath(input string) []string {
+	return strings.Split(input, ",")
+}
+
+func parseToken(input string) (string, int) {
+	direction := input[:1]
+	distance, _ := strconv.Atoi(input[1:])
+	return direction, distance
+}
+
+func goLeft(x *int, y int, distance int, wireNumber int, m map[point][]int) {
+	for i := 1; i <= distance; i++ {
+		*x--
+		m[point{*x, y}] = append(m[point{*x, y}], wireNumber)
+	}
+}
+
+func goRight(x *int, y int, distance int, wireNumber int, m map[point][]int) {
+	for i := 1; i <= distance; i++ {
+		*x++
+		m[point{*x, y}] = append(m[point{*x, y}], wireNumber)
+	}
+}
+
+func goUp(x int, y *int, distance int, wireNumber int, m map[point][]int) {
+	for i := 1; i <= distance; i++ {
+		*y++
+		m[point{x, *y}] = append(m[point{x, *y}], wireNumber)
+	}
+}
+
+func goDown(x int, y *int, distance int, wireNumber int, m map[point][]int) {
+	for i := 1; i <= distance; i++ {
+		*y--
+		m[point{x, *y}] = append(m[point{x, *y}], wireNumber)
+	}
+}
+
+func tracePath(tokens []string, wireNumber int, m map[point][]int) {
+	m[point{0, 0}] = append(m[point{0, 0}], wireNumber)
+
+	x, y := 0, 0
+	for _, token := range tokens {
+		direction, distance := parseToken(token)
+		switch direction {
+		case "R":
+			goRight(&x, y, distance, wireNumber, m)
+		case "L":
+			goLeft(&x, y, distance, wireNumber, m)
+		case "U":
+			goUp(x, &y, distance, wireNumber, m)
+		case "D":
+			goDown(x, &y, distance, wireNumber, m)
+		}
 	}
 }
