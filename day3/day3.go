cd3683e99d2b2f3c3bad797b500e49c115a7ff84
Adam Miller <millera023@gmail.com>
2019-12-08 00:48:43 -0500

    day3 part 2 solved

--- day3/day3.go
+++ day3/day3.go
@@ -9,7 +9,7 @@ import (
 
 // Run day 3 of advent
 func Run(input <-chan string) {
-	m := make(map[point][]int)
+	m := make(map[point][]wire)
 
 	wireNumber := 0
 	for wirePath := range input {
@@ -21,10 +21,15 @@ func Run(input <-chan string) {
 	// point 0,0 will always have the wires connected
 	delete(m, point{0, 0})
 
+	fmt.Println("Day3 -- Part1: ", part1(m))
+	fmt.Println("Day3 -- Part2: ", part2(m))
+}
+
+func part1(m map[point][]wire) int {
 	minSeenDistance := math.MaxInt32
 	for key, value := range m {
 		if len(value) == 2 { // Assuming that there were be at most 2 values per point in space
-			if value[0] != value[1] {
+			if value[0].Number != value[1].Number {
 				distance := abs(key.X) + abs(key.Y)
 				if distance < minSeenDistance {
 					minSeenDistance = distance
@@ -33,7 +38,23 @@ func Run(input <-chan string) {
 		}
 	}
 
-	fmt.Println("Day3 -- Part1: ", minSeenDistance)
+	return minSeenDistance
+}
+
+func part2(m map[point][]wire) int {
+	minSeenSteps := math.MaxInt32
+	for _, value := range m {
+		if len(value) == 2 { // Assuming that there were be at most 2 values per point in space
+			if value[0].Number != value[1].Number {
+				distance := value[0].Steps + value[1].Steps
+				if distance < minSeenSteps {
+					minSeenSteps = distance
+				}
+			}
+		}
+	}
+
+	return minSeenSteps
 }
 
 func abs(i int) int {
@@ -47,6 +68,10 @@ type point struct {
 	X, Y int
 }
 
+type wire struct {
+	Number, Steps int
+}
+
 func tokenizePath(input string) []string {
 	return strings.Split(input, ",")
 }
@@ -57,49 +82,37 @@ func parseToken(input string) (string, int) {
 	return direction, distance
 }
 
-func goLeft(x *int, y int, distance int, wireNumber int, m map[point][]int) {
-	for i := 1; i <= distance; i++ {
-		*x--
-		m[point{*x, y}] = append(m[point{*x, y}], wireNumber)
-	}
-}
+func tracePath(tokens []string, wireNumber int, m map[point][]wire) {
+	m[point{0, 0}] = append(m[point{0, 0}], wire{wireNumber, 0})
 
-func goRight(x *int, y int, distance int, wireNumber int, m map[point][]int) {
-	for i := 1; i <= distance; i++ {
-		*x++
-		m[point{*x, y}] = append(m[point{*x, y}], wireNumber)
-	}
-}
-
-func goUp(x int, y *int, distance int, wireNumber int, m map[point][]int) {
-	for i := 1; i <= distance; i++ {
-		*y++
-		m[point{x, *y}] = append(m[point{x, *y}], wireNumber)
-	}
-}
-
-func goDown(x int, y *int, distance int, wireNumber int, m map[point][]int) {
-	for i := 1; i <= distance; i++ {
-		*y--
-		m[point{x, *y}] = append(m[point{x, *y}], wireNumber)
-	}
-}
-
-func tracePath(tokens []string, wireNumber int, m map[point][]int) {
-	m[point{0, 0}] = append(m[point{0, 0}], wireNumber)
-
-	x, y := 0, 0
+	x, y, stepCount := 0, 0, 0
 	for _, token := range tokens {
 		direction, distance := parseToken(token)
 		switch direction {
 		case "R":
-			goRight(&x, y, distance, wireNumber, m)
+			for i := 1; i <= distance; i++ {
+				x++
+				stepCount++
+				m[point{x, y}] = append(m[point{x, y}], wire{wireNumber, stepCount})
+			}
 		case "L":
-			goLeft(&x, y, distance, wireNumber, m)
+			for i := 1; i <= distance; i++ {
+				x--
+				stepCount++
+				m[point{x, y}] = append(m[point{x, y}], wire{wireNumber, stepCount})
+			}
 		case "U":
-			goUp(x, &y, distance, wireNumber, m)
+			for i := 1; i <= distance; i++ {
+				y++
+				stepCount++
+				m[point{x, y}] = append(m[point{x, y}], wire{wireNumber, stepCount})
+			}
 		case "D":
-			goDown(x, &y, distance, wireNumber, m)
+			for i := 1; i <= distance; i++ {
+				y--
+				stepCount++
+				m[point{x, y}] = append(m[point{x, y}], wire{wireNumber, stepCount})
+			}
 		}
 	}
 }
