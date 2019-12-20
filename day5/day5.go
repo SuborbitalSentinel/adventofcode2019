7933e9f352274d490a8357279a8255ffd00ca6a1
Adam Miller <millera023@gmail.com>
2019-12-19 20:55:31 -0500

    Day 5 part 1 complete

--- day5/day5.go
+++ day5/day5.go
@@ -1,8 +1,11 @@
 package day5
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/millera023/adventofcode/util"
 )
@@ -12,7 +15,8 @@ func Run(input <-chan string) {
 	intcodes := util.ChannelToSlice(input)
 
 	// Part 1
-	fmt.Println("Day5 -- Part1: ", executeProgram(intcodes))
+	fmt.Println("Day5 -- Part1: ")
+	executeProgram(intcodes)
 }
 
 func splitOpCode(opCode string) (string, []string) {
@@ -26,48 +30,79 @@ func splitOpCode(opCode string) (string, []string) {
 }
 
 func processOpCode(opCodeIndex int, program []string) int {
+	const alu, io = 4, 2
 	currentOpCode, paramModes := splitOpCode(program[opCodeIndex])
 
 	switch currentOpCode {
+
 	case "01":
 		paramMode1 := paramModes[0]
 		paramMode2 := paramModes[1]
 		reg1, reg2 := getRegisterValues(program, program[opCodeIndex+1], program[opCodeIndex+2], paramMode1, paramMode2)
-		return reg1 + reg2
+
+		program[outputIndex(program, opCodeIndex+3)] = strconv.Itoa(reg1 + reg2)
+		return alu
+
 	case "02":
 		paramMode1 := paramModes[0]
 		paramMode2 := paramModes[1]
 		reg1, reg2 := getRegisterValues(program, program[opCodeIndex+1], program[opCodeIndex+2], paramMode1, paramMode2)
-		return reg1 * reg2
+
+		program[outputIndex(program, opCodeIndex+3)] = strconv.Itoa(reg1 * reg2)
+		return alu
+
+	case "03":
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Input Requested: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.Replace(input, "\n", "", -1)
+
+		program[outputIndex(program, opCodeIndex+1)] = input
+		return io
+
+	case "04":
+		paramMode1 := paramModes[0]
+		if paramMode1 == "1" {
+			fmt.Println("Output: ", program[opCodeIndex+1])
+		} else {
+			fmt.Println("Output: ", program[outputIndex(program, opCodeIndex+1)])
+		}
+
+		return io
+
 	default:
 		panic(fmt.Sprintf("%v %v", "Bad Op Code:", program[opCodeIndex]))
 	}
+}
 
+func outputIndex(program []string, programPointer int) int {
+	outputRegisterIndex, _ := strconv.Atoi(program[programPointer])
+
+	return outputRegisterIndex
 }
 
 func getRegisterValues(program []string, r1 string, r2 string, paramMode1 string, paramMode2 string) (int, int) {
 	reg1, _ := strconv.Atoi(r1)
 	reg2, _ := strconv.Atoi(r2)
 
-	if paramMode1 == "1" {
+	if paramMode1 == "0" {
 		reg1, _ = strconv.Atoi(program[reg1])
 	}
-	if paramMode2 == "1" {
+	if paramMode2 == "0" {
 		reg2, _ = strconv.Atoi(program[reg2])
 	}
 
 	return reg1, reg2
 }
 
-func executeProgram(program []string) string {
+func executeProgram(program []string) {
 	currentOpCodeIndex := 0
 	for {
 		if program[currentOpCodeIndex] == "99" {
+			fmt.Println("Program Halt")
 			break
 		}
 
 		currentOpCodeIndex += processOpCode(currentOpCodeIndex, program)
 	}
-
-	return program[0]
 }
--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"github.com/millera023/adventofcode/day1"
-	"github.com/millera023/adventofcode/day2"
-	"github.com/millera023/adventofcode/day3"
-	"github.com/millera023/adventofcode/day4"
 	"github.com/millera023/adventofcode/day5"
 	"github.com/millera023/adventofcode/util"
 )
 
 func main() {
-	day1.Run(util.Readlines("day1/input"))
-	day2.Run(util.Readwords("day2/input"))
-	day3.Run(util.Readlines("day3/input"))
-	day4.Run() // No input required
+	//day1.Run(util.Readlines("day1/input"))
+	//day2.Run(util.Readwords("day2/input"))
+	//day3.Run(util.Readlines("day3/input"))
+	//day4.Run() // No input required
 	day5.Run(util.Readwords("day5/input"))
 }
